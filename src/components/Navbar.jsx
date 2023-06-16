348b731fe43c192dc29434cd7bd94b214e61a458
febrydjr <febrydj99@gmail.com>
2023-06-16 11:26:57 +0700

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
-import { Box, Flex, Link, Spacer, Image, Avatar } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Link,
+  Spacer,
+  Image,
+  Avatar,
+  IconButton,
+  Collapse,
+} from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <Box bg="gray.800" py={4} px={8}>
       <Flex align="center" justify="space-between" color="white">
@@ -22,7 +40,7 @@ function Navbar() {
             MAI BLOGGGGG
           </Link>
         </Flex>
-        <Flex align="center">
+        <Flex display={{ base: "none", md: "flex" }} align="center">
           <Link
             as={RouterLink}
             to="/articles/popular"
@@ -71,7 +89,65 @@ function Navbar() {
             />
           </Link>
         </Flex>
+        <IconButton
+          aria-label="Toggle navigation"
+          icon={isOpen ? <AiOutlineClose /> : <GiHamburgerMenu />}
+          onClick={handleToggle}
+          display={{ base: "flex", md: "none" }}
+        />
       </Flex>
+      <Collapse in={isOpen} animateOpacity>
+        <Flex direction="column" mt={4} color="white">
+          <Link
+            as={RouterLink}
+            to="/articles/popular"
+            mb={2}
+            _hover={{ textDecoration: "none" }}
+          >
+            Popular Articles
+          </Link>
+          <Link
+            as={RouterLink}
+            to="/create-article"
+            mb={2}
+            _hover={{ textDecoration: "none" }}
+          >
+            Create Article
+          </Link>
+          <Link
+            as={RouterLink}
+            to="/myblogs"
+            mb={2}
+            _hover={{ textDecoration: "none" }}
+          >
+            My Blog
+          </Link>
+          <Link
+            as={RouterLink}
+            to="/login"
+            mb={2}
+            _hover={{ textDecoration: "none" }}
+          >
+            Log In
+          </Link>
+          <Link
+            as={RouterLink}
+            to="/register"
+            mb={2}
+            _hover={{ textDecoration: "none" }}
+          >
+            Register
+          </Link>
+          <Link as={RouterLink} to="/profile">
+            <Avatar
+              name="User"
+              src="path/to/profile-picture"
+              size="sm"
+              mb={2}
+            />
+          </Link>
+        </Flex>
+      </Collapse>
     </Box>
   );
 }
