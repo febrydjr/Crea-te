37107cfc45a741521538c30d31ad6d665f3b9cb6
febrydjr <febrydj99@gmail.com>
2023-06-16 10:40:20 +0700

    read more is ready, article pages

--- src/data/articles.js
+++ src/data/articles.js
@@ -5,6 +5,7 @@ const articlesData = [
     category: "Sports",
     title: "Indonesia Juara Piala Dunia 2022, Cuti Bersama 3 Hari",
     author: "Febry Dharmawan Jr",
+    date: "2022-01-01",
     content:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod mauris vel felis bibendum, at pretium urna bibendum. Vivamus ut luctus nulla, id tincidunt nisl. Duis in ante vel elit viverra bibendum. Maecenas aliquet, velit id fringilla ultrices, dui sapien sollicitudin velit, vel pulvinar mi velit eu sapien.",
   },
@@ -14,8 +15,11 @@ const articlesData = [
     category: "Technology",
     title: "React Itu Mudah Sekali, Semua Orang Pasti Bisa (Mungkin)",
     author: "Andre D Java",
+    date: "20 Oktober 2023",
+    videos: "https://www.youtube.com/embed/6Lzv5XKl6H8",
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod mauris vel felis bibendum, at pretium urna bibendum. Vivamus ut luctus nulla, id tincidunt nisl. Duis in ante vel elit viverra bibendum. Maecenas aliquet, velit id fringilla ultrices, dui sapien sollicitudin velit, vel pulvinar mi velit eu sapien.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque semper commodo odio. Donec feugiat neque quis velit malesuada tristique. In pellentesque purus a pretium commodo. Nulla sit amet ultricies velit. Duis sed purus ac lacus aliquam auctor. Ut sagittis porta molestie. Fusce id lacinia libero, a malesuada libero. Praesent mattis leo eu ligula sagittis luctus. Suspendisse sed accumsan lacus. Suspendisse iaculis porttitor quam et pellentesque. Curabitur varius vestibulum nisi, vel faucibus nisi volutpat quis. Nunc tristique ex et libero venenatis, id gravida arcu scelerisque. Pellentesque sit amet volutpat justo. Nulla facilisi. Nunc et diam varius orci facilisis maximus non vel elit. Phasellus mollis dictum risus, sed vestibulum nisl.Ut at maximus nisi. Nulla pulvinar orci vel tellus varius lobortis. Praesent mollis eros vel dolor gravida aliquam. Cras tempus tellus a massa bibendum, sit amet imperdiet neque auctor. Etiam mattis elit ac dui gravida tempor. Curabitur nulla elit, aliquet et nisl sit amet, elementum porta tortor. Nam finibus dictum pellentesque. Proin enim lectus, lacinia eget felis id, gravida porta nunc. Donec vulputate felis vitae consequat ornare. Integer lacus ligula, sollicitudin at massa eu, rhoncus varius arcu. Sed facilisis, velit ut elementum tincidunt, metus nisl pharetra diam, vitae volutpat nibh erat vitae lectus. Phasellus laoreet justo non libero sagittis porttitor. Morbi vel tristique velit. Vivamus interdum leo risus, ut sollicitudin elit tempus in. Nunc sed ipsum fermentum, rhoncus mauris quis, feugiat purus.Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in sodales ante, non vehicula metus. Sed sit amet augue pharetra, interdum urna vitae, tristique ipsum. Donec blandit eros leo, a fringilla risus porttitor nec. Morbi sodales nisi a metus pharetra molestie. Ut lobortis consectetur justo. Donec nec finibus massa. Nullam cursus purus at vehicula varius.Vivamus sollicitudin sem non interdum cursus. Pellentesque id blandit nulla. Phasellus at enim sed risus imperdiet dictum. Morbi eget justo ante. Proin molestie felis sed aliquam condimentum. Nullam quis aliquam est. Mauris rhoncus turpis nisl, sed egestas ante mollis vitae.",
+    keywords: "React, Purwadhika, Belajar, JavaScript",
   },
   {
     id: 3,
@@ -34,6 +38,7 @@ const articlesData = [
     author: "Najwa Shihab",
     content:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod mauris vel felis bibendum, at pretium urna bibendum. Vivamus ut luctus nulla, id tincidunt nisl. Duis in ante vel elit viverra bibendum. Maecenas aliquet, velit id fringilla ultrices, dui sapien sollicitudin velit, vel pulvinar mi velit eu sapien.",
+    keywords: "Business",
   },
   {
     id: 5,
--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,200 +1,62 @@
-import { useParams } from "react-router-dom";
-import React, { useEffect, useState } from "react";
-import { Link, Navigate, useNavigate } from "react-router-dom";
-
-import {
-  Box,
-  Heading,
-  Text,
-  Image,
-  Flex,
-  Button,
-  InputGroup,
-  InputLeftElement,
-  Input,
-  Select,
-  useToast,
-  InputRightElement,
-} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Box, Image, Text, Button, Flex, VStack } from "@chakra-ui/react";
 import articlesData from "../data/articles";
 
-function ArticlePage() {
-  const { articleId } = useParams();
-  const [sortBy, setSortBy] = useState("newest");
-  const [filterBy, setFilterBy] = useState("all");
-  const [searchQuery, setSearchQuery] = useState("");
-  const toast = useToast();
+const ArticlePage = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const articlesPerPage = 1;
+  const totalArticles = articlesData.length;
+  const totalPages = Math.ceil(totalArticles / articlesPerPage);
+  const indexOfLastArticle = currentPage * articlesPerPage;
+  const indexOfFirstArticle = indexOfLastArticle - articlesPerPage;
+  const currentArticle = articlesData[indexOfFirstArticle];
 
-  const article = articlesData.find((article) => article.id === articleId);
-
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  //PAKE USE EFFECT INI
-  useEffect(() => {
-    const isAuthenticated = localStorage.getItem("isAuthenticated");
-    setIsAuthenticated(!!isAuthenticated);
-  }, []);
-
-  if (isAuthenticated) {
-    return <Navigate to="/" />;
-  }
-  const handleSortChange = (event) => {
-    setSortBy(event.target.value);
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
   };
 
-  const handleFilterChange = (event) => {
-    setFilterBy(event.target.value);
+  const renderPageNumbers = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(
+        <Button
+          key={i}
+          onClick={() => handlePageChange(i)}
+          // variant={i === currentPage ? "teal" : "outline"}
+          colorScheme={i === currentPage ? "teal" : "gray"}
+          mx={1}
+          mb={4}
+        >
+          {i}
+        </Button>
+      );
+    }
+    return pageNumbers;
   };
-
-  const handleSearchSubmit = (event) => {
-    event.preventDefault();
-    if (searchQuery.trim() !== "") {
-      toast({
-        title: `Searching for articles with "${searchQuery}"`,
-        status: "info",
-        duration: 2000,
-        isClosable: true,
-      });
-      setSearchQuery("");
-    }
-  };
-
-  const filteredArticles = articlesData.filter((article) => {
-    if (filterBy === "all" || article.category === filterBy) {
-      if (searchQuery.trim() === "") {
-        return true;
-      } else {
-        return (
-          article.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-          article.content.toLowerCase().includes(searchQuery.toLowerCase()) ||
-          article.keywords.some((keyword) =>
-            keyword.toLowerCase().includes(searchQuery.toLowerCase())
-          )
-        );
-      }
-    } else {
-      return false;
-    }
-  });
-
-  const sortedArticles = filteredArticles.sort((a, b) => {
-    if (sortBy === "newest") {
-      return new Date(b.date) - new Date(a.date);
-    } else {
-      return new Date(a.date) - new Date(b.date);
-    }
-  });
 
   return (
-    <Box px={6} py={4}>
-      <Box mb={6}>
-        <Heading as="h1" size="xl">
-          {article.title}
-        </Heading>
-        <Text fontSize="lg" color="gray.500" mb={2}>
-          By {article.author} on {new Date(article.date).toLocaleDateString()}
-        </Text>
-        <Image src={article.image} alt={article.title} mb={4} />
-        <Text fontSize="xl" mb={6}>
-          {article.content}
-        </Text>
-        {article.videos?.map((video) => (
-          <Box key={video} mb={4}>
-            <iframe
-              width="100%"
-              height="400"
-              src={video}
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
+    <Box p={4}>
+      <Box maxW="1100px" mx="auto" mb={6}>
+        <Box borderWidth="1px" borderRadius="lg" overflow="hidden">
+          <Image src={currentArticle.thumbnail} alt={currentArticle.title} />
+          <Box p={4}>
+            <Text fontSize="xl" fontWeight="bold" mb={2}>
+              {currentArticle.title}
+            </Text>
+            <Text fontSize="md" fontStyle="italic" mb={4}>
+              By {currentArticle.author}
+              <Text>Published: {currentArticle.date}</Text>
+            </Text>
+            <Text mb={4}>{currentArticle.content}</Text>
+            Keyword: {currentArticle.keywords}
           </Box>
-        ))}
+        </Box>
       </Box>
-      <Box mb={6}>
-        <Heading as="h2" size="lg" mb={2}>
-          Related Articles
-        </Heading>
-        <Flex wrap="wrap">
-          {sortedArticles?.map((article) => (
-            <Box
-              key={article.id}
-              width={{ base: "100%", md: "50%", lg: "33.33%" }}
-              mb={4}
-            >
-              <Image src={article.image} alt={article.title} mb={2} />
-              <Heading as="h3" size="md" mb={2}>
-                {article.title}
-              </Heading>
-              <Text fontSize="sm" color="gray.500" mb={2}>
-                By {article.author} on{" "}
-                {new Date(article.date).toLocaleDateString()}
-              </Text>
-              <Button
-                as="a"
-                href={`/articles/${article.id}`}
-                size="sm"
-                variant="outline"
-                mb={2}
-              >
-                Read More
-              </Button>
-            </Box>
-          ))}
-        </Flex>
-      </Box>
-      <Box mb={6}>
-        <Heading as="h2" size="lg" mb={2}>
-          Filter and Sort Articles
-        </Heading>
-        <Flex wrap="wrap">
-          <Box
-            width={{ base: "100%", md: "50%" }}
-            mr={{ md: 4 }}
-            mb={{ base: 4, md: 0 }}
-          >
-            <InputGroup size="md">
-              <InputLeftElement
-                pointerEvents="none"
-                children={<i className="fas fa-search"></i>}
-              />
-              <Input
-                type="text"
-                placeholder="Search articles..."
-                value={searchQuery}
-                onChange={(event) => setSearchQuery(event.target.value)}
-              />
-              <InputRightElement>
-                <Button size="sm" onClick={handleSearchSubmit}>
-                  Search
-                </Button>
-              </InputRightElement>
-            </InputGroup>
-          </Box>
-          <Box width={{ base: "100%", md: "50%" }}>
-            <Select
-              size="md"
-              value={sortBy}
-              onChange={handleSortChange}
-              mb={{ base: 4, md: 0 }}
-              mr={{ md: 4 }}
-            >
-              <option value="newest">Newest First</option>
-              <option value="oldest">Oldest First</option>
-            </Select>
-            <Select size="md" value={filterBy} onChange={handleFilterChange}>
-              <option value="all">All Categories</option>
-              <option value="technology">Technology</option>
-              <option value="sports">Sports</option>
-              <option value="politics">Politics</option>
-              <option value="entertainment">Entertainment</option>
-            </Select>
-          </Box>
-        </Flex>
-      </Box>
+      <Flex justifyContent="center" alignItems="center">
+        {renderPageNumbers()}
+      </Flex>
     </Box>
   );
-}
+};
 
 export default ArticlePage;
