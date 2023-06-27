214270a1fb2de4308e6cf558638421468a01ee80
febrydjr <febrydj99@gmail.com>
2023-06-27 20:28:37 +0700

    create undone

--- src/pages/CreateArticlePage.jsx
+++ src/pages/CreateArticlePage.jsx
@@ -2,21 +2,23 @@ import React, { useEffect, useState } from "react";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-
 import {
   Box,
   Heading,
   FormControl,
   FormLabel,
   Input,
-  Textarea,
-  Select,
   Button,
   Text,
   useToast,
   Image,
+  Select,
 } from "@chakra-ui/react";
-import articlesData from "../data/articles";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import TagsInput from "react-tagsinput";
+import "react-tagsinput/react-tagsinput.css";
 
 function withAuth(Component) {
   return function WrappedComponent(props) {
@@ -30,267 +32,290 @@ function withAuth(Component) {
     }, [isAuthenticated, navigate]);
 
     if (!isAuthenticated) {
-      return null;
+      return null; // or any other placeholder while checking authentication
     }
 
     return <Component {...props} />;
   };
 }
 
-function CreateArticlePage() {
-  const navigate = useNavigate();
-  const [title, setTitle] = useState("");
-  const [author, setAuthor] = useState("");
-  const [date, setDate] = useState("");
-  const [thumbnail, setThumbnail] = useState(null);
-  const [category, setCategory] = useState("technology");
-  const [content, setContent] = useState("");
-  const [videos, setVideos] = useState([]);
-  const [keywords, setKeywords] = useState([]);
-  const [isCreated, setIsCreated] = useState(false);
-  const [selectedFileName, setSelectedFileName] = useState("");
-  const [thumbnailPreview, setThumbnailPreview] = useState(null);
+const CreateArticlePage = () => {
   const toast = useToast();
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [categories, setCategories] = useState([]);
 
-  const handleTitleChange = (event) => {
-    setTitle(event.target.value);
-  };
-
-  const handleAuthorChange = (event) => {
-    setAuthor(event.target.value);
-  };
-
-  const handleDateChange = (event) => {
-    setDate(event.target.value);
-  };
-
-  const handleThumbnailChange = (event) => {
-    const file = event.target.files[0];
-    if (file && file.size <= 2 * 1024 * 1024) {
-      // Check if file size is within the limit (2MB)
-      setThumbnail(file);
-      setSelectedFileName(file.name);
-      setThumbnailPreview(URL.createObjectURL(file)); // Create a preview URL for the thumbnail
-    } else {
-      // Handle file size exceeded error
-      toast({
-        title: "File size limit exceeded.",
-        description: "Please upload a file up to 2MB in size.",
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-      });
-    }
-  };
-
-  const handleCategoryChange = (event) => {
-    setCategory(event.target.value);
-  };
-
-  const handleContentChange = (value) => {
-    setContent(value);
-  };
-
-  const handleVideoAdd = (event) => {
-    event.preventDefault();
-    setVideos([...videos, ""]);
-  };
-
-  const handleVideoUpload = (event, index) => {
-    const file = event.target.files[0];
-    if (file && file.size <= 100 * 1024 * 1024) {
-      // Check if file size is within the limit (100MB)
-      const newVideos = [...videos];
-      newVideos[index] = URL.createObjectURL(file);
-      setVideos(newVideos);
-    } else {
-      // Handle file size exceeded error
-      toast({
-        title: "File size limit exceeded.",
-        description: "Please upload a file up to 100MB in size.",
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-      });
-    }
-  };
-
-  const handleVideoChange = (event, index) => {
-    const newVideos = [...videos];
-    newVideos[index] = event.target.value;
-    setVideos(newVideos);
-  };
-
-  const handleKeywordAdd = (event) => {
-    event.preventDefault();
-    setKeywords([...keywords, ""]);
-  };
-
-  const handleKeywordChange = (event, index) => {
-    const newKeywords = [...keywords];
-    newKeywords[index] = event.target.value;
-    setKeywords(newKeywords);
-  };
-
-  const handleCreateSubmit = (event) => {
-    event.preventDefault();
-    const newArticle = {
-      id: String(Date.now()),
-      title,
-      author,
-      date,
-      thumbnail: URL.createObjectURL(thumbnail),
-      category,
-      content,
-      videos: videos.filter((video) => video.trim() !== ""),
-      keywords: keywords.filter((keyword) => keyword.trim() !== ""),
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get(
+          "https://minpro-blog.purwadhikabootcamp.com/api/auth",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const { username: fetchedUsername, avatar: fetchedAvatar } =
+          response.data;
+        setUsername(fetchedUsername);
+        setAvatar(fetchedAvatar);
+      } catch (error) {
+        console.log(error);
+      }
     };
-    articlesData.push(newArticle);
-    setIsCreated(true);
-    toast({
-      title: "Article created!",
-      status: "success",
-      duration: 2000,
-      isClosable: true,
-    });
-    navigate("/");
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get(
+          "https://minpro-blog.purwadhikabootcamp.com/api/blog/allCategory"
+        );
+        setCategories(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchCategories();
+  }, []);
+
+  const initialValues = {
+    title: "",
+    imageURL: null,
+    category: "",
+    content: "",
+    keywords: [],
+  };
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required("Title is required"),
+    imageURL: Yup.mixed()
+      .required("imageURL is required")
+      .test("fileSize", "File size limit exceeded", (value) => {
+        if (value) {
+          return value.size <= 2 * 1024 * 1024;
+        }
+        return true;
+      }),
+    category: Yup.string().required("Category is required"),
+    content: Yup.string().required("Content is required"),
+    keywords: Yup.array().min(1, "At least one keyword is required"),
+  });
+
+  const handleCreateSubmit = async (values, { setSubmitting }) => {
+    try {
+      const formData = new FormData();
+      formData.append("title", values.title);
+      formData.append("imageURL", values.imageURL);
+      // formData.append("imageURL", values.imageURL[0]);
+      formData.append("category", values.category);
+      formData.append("content", values.content);
+      values.keywords.forEach((keyword) =>
+        formData.append("keywords[]", keyword)
+      );
+
+      const token = localStorage.getItem("token");
+      const response = await axios.post(
+        "https://minpro-blog.purwadhikabootcamp.com/api/blog",
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      console.log(response);
+
+      setSubmitting(false);
+      toast({
+        title: "Article created!",
+        status: "success",
+        duration: 2000,
+        isClosable: true,
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      setSubmitting(false);
+      toast({
+        title: "Error creating article.",
+        description: "An error occurred while creating the article.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
   };
 
   return (
-    <Box
-      fontFamily={"monospace"}
-      w={"80%"}
-      m={"auto"}
-      mb={10}
-      mt={10}
-      px={6}
-      py={4}
-    >
-      <Heading fontFamily={"monospace"} as="h1" size="xl" mb={4}>
+    <Box fontFamily="monospace" w="80%" m="auto" mb={10} mt={10} px={6} py={4}>
+      <Heading fontFamily="monospace" as="h1" size="xl" mb={4}>
         Create Article
       </Heading>
-      <form onSubmit={handleCreateSubmit}>
-        <FormControl id="title" mb={4}>
-          <FormLabel fontWeight={"bold"}>Title</FormLabel>
-          <Input
-            type="text"
-            value={title}
-            onChange={handleTitleChange}
-            required
-          />
-        </FormControl>
-        <FormControl id="author" mb={4}>
-          <FormLabel fontWeight={"bold"}>Author</FormLabel>
-          <Input
-            type="text"
-            value={author}
-            onChange={handleAuthorChange}
-            required
-          />
-        </FormControl>
-        <FormControl id="date" mb={4}>
-          <FormLabel fontWeight={"bold"}>Publication Date</FormLabel>
-          <Input
-            type="date"
-            value={date}
-            onChange={handleDateChange}
-            required
-          />
-        </FormControl>
-        <FormControl id="thumbnail" mb={4}>
-          <FormLabel fontWeight="bold">Thumbnail</FormLabel>
-          <Button
-            as="label"
-            htmlFor="thumbnailInput"
-            size="sm"
-            variant="outline"
-            colorScheme="teal"
-            cursor="pointer"
-          >
-            Upload
-            <Input
-              id="thumbnailInput"
-              type="file"
-              accept=".jpg, .jpeg, .png"
-              onChange={handleThumbnailChange}
-              display="none"
-              required
-            />
-          </Button>
-          {thumbnailPreview && (
-            <Box mt={2}>
-              <Image src={thumbnailPreview} maxH="200px" />
-            </Box>
-          )}
-        </FormControl>
-        <FormControl id="category" mb={4}>
-          <FormLabel fontWeight={"bold"}>Category</FormLabel>
-          <Select value={category} onChange={handleCategoryChange} required>
-            <option value="technology">Technology</option>
-            <option value="sports">Sports</option>
-            <option value="politics">Politics</option>
-            <option value="entertainment">Entertainment</option>
-          </Select>
-        </FormControl>
-        <FormControl id="content" mb={4}>
-          <FormLabel fontWeight={"bold"}>News Content</FormLabel>
-          <div className="text-editor">
-            <ReactQuill
-              theme="snow"
-              value={content}
-              onChange={handleContentChange}
-              required
-            />
-          </div>
-        </FormControl>
-        <FormControl id="videos" mb={4}>
-          <FormLabel fontWeight={"bold"}>Videos</FormLabel>
-          {videos.map((video, index) => (
-            <Box key={index} mb={2}>
-              {video ? (
-                <video src={video} controls width={300} height={200} />
-              ) : (
-                <Input
-                  type="file"
-                  accept="video/*"
-                  onChange={(event) => handleVideoUpload(event, index)}
-                  required
-                />
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleCreateSubmit}
+      >
+        {(formik) => (
+          <Form>
+            <Field name="title">
+              {({ field, form }) => (
+                <FormControl
+                  id="title"
+                  mb={4}
+                  isInvalid={form.errors.title && form.touched.title}
+                >
+                  <FormLabel fontWeight="bold">Title</FormLabel>
+                  <Input type="text" {...field} />
+                  <ErrorMessage
+                    name="title"
+                    component={Text}
+                    color="red.500"
+                    mt={1}
+                  />
+                </FormControl>
               )}
-            </Box>
-          ))}
-          <Button size="sm" variant="outline" onClick={handleVideoAdd}>
-            Add Video
-          </Button>
-        </FormControl>
-        <FormControl id="keywords" mb={4}>
-          <FormLabel fontWeight={"bold"}>Keywords</FormLabel>
-          {keywords.map((keyword, index) => (
-            <Box key={index} mb={2}>
-              <Input
-                type="text"
-                value={keyword}
-                onChange={(event) => handleKeywordChange(event, index)}
-                required
-              />
-            </Box>
-          ))}
-          <Button size="sm" variant="outline" onClick={handleKeywordAdd}>
-            Add Keyword
-          </Button>
-        </FormControl>
-        <Button
-          mt={4}
-          colorScheme="teal"
-          type="submit"
-          size="md"
-          variant="solid"
-        >
-          Create Article
-        </Button>
-      </form>
+            </Field>
+            <Field name="imageURL">
+              {({ field, form }) => (
+                <FormControl
+                  id="imageURL"
+                  mb={4}
+                  isInvalid={form.errors.imageURL && form.touched.imageURL}
+                >
+                  <FormLabel fontWeight="bold">imageURL</FormLabel>
+                  <input
+                    id="imageURLInput"
+                    type="file"
+                    accept=".jpg, .jpeg, .png"
+                    onChange={(event) =>
+                      form.setFieldValue(
+                        "imageURL",
+                        event.currentTarget.files[0]
+                      )
+                    }
+                    style={{ display: "none" }}
+                  />
+                  <label htmlFor="imageURLInput">
+                    <Button
+                      as="span"
+                      size="sm"
+                      variant="outline"
+                      colorScheme="teal"
+                      cursor="pointer"
+                    >
+                      Upload
+                    </Button>
+                  </label>
+                  {formik.values.imageURL && (
+                    <Box mt={2}>
+                      <Image
+                        src={URL.createObjectURL(formik.values.imageURL)}
+                        maxH="200px"
+                      />
+                    </Box>
+                  )}
+                  <ErrorMessage
+                    name="imageURL"
+                    component={Text}
+                    color="red.500"
+                    mt={1}
+                  />
+                </FormControl>
+              )}
+            </Field>
+            <Field name="category">
+              {({ field, form }) => (
+                <FormControl
+                  id="category"
+                  mb={4}
+                  isInvalid={form.errors.category && form.touched.category}
+                >
+                  <FormLabel fontWeight="bold">Category</FormLabel>
+                  <Select {...field} placeholder="Select category">
+                    {categories.map((category) => (
+                      <option key={category.id} value={category.name}>
+                        {category.name}
+                      </option>
+                    ))}
+                  </Select>
+
+                  <ErrorMessage
+                    name="category"
+                    component={Text}
+                    color="red.500"
+                    mt={1}
+                  />
+                </FormControl>
+              )}
+            </Field>
+
+            <div>
+              <FormControl
+                id="content"
+                mb={4}
+                isInvalid={formik.errors.content && formik.touched.content}
+              >
+                <FormLabel fontWeight="bold">News Content</FormLabel>
+                <div className="text-editor">
+                  <ReactQuill
+                    theme="snow"
+                    value={formik.values.content}
+                    onChange={(value) => formik.setFieldValue("content", value)}
+                    onBlur={() => formik.setFieldTouched("content", true)}
+                  />
+                </div>
+                <ErrorMessage
+                  name="content"
+                  component={Text}
+                  color="red.500"
+                  mt={1}
+                />
+              </FormControl>
+            </div>
+
+            <Field name="keywords">
+              {({ field, form }) => (
+                <FormControl
+                  id="keywords"
+                  mb={4}
+                  isInvalid={form.errors.keywords && form.touched.keywords}
+                >
+                  <FormLabel fontWeight="bold">Keywords</FormLabel>
+                  <TagsInput
+                    value={field.value}
+                    onChange={(tags) => form.setFieldValue("keywords", tags)}
+                  />
+                  <ErrorMessage
+                    name="keywords"
+                    component={Text}
+                    color="red.500"
+                    mt={1}
+                  />
+                </FormControl>
+              )}
+            </Field>
+            <Button
+              mt={4}
+              colorScheme="teal"
+              type="submit"
+              size="md"
+              variant="solid"
+              isLoading={formik.isSubmitting}
+            >
+              Create Article
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </Box>
   );
-}
+};
 
 export default withAuth(CreateArticlePage);
